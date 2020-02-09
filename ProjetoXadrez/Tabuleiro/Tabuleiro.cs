9006e3a5ec2f8ef055415b8043a27decc4111b17
Guilherme Almeida <guireis_almeida@hotmail.com>
2020-02-09 16:22:30 -0300

    Começando a implementar tabuleiro

---
+++ ProjetoXadrez/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int Linhas, int Colunas)
+        {
+            this.linhas = Linhas;
+            this.colunas = Colunas;
+            this.pecas = new Peca[linhas, colunas];
+        }
+    }
+}
